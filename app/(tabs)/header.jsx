fcbdaaa8de2f23b8614de039da07c80809aa77d7
HJohn0360 <john.harris-7@selu.edu>
2024-09-15 23:24:07 -0500

    This is some more stuff

---
+++ app/(tabs)/header.jsx
@@ -0,0 +1,15 @@
+function header(){
+
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    </ul>
+            </nav>
+        </header>
+        );
+    }
+
+export default header
